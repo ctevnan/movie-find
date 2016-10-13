83ab0d909b2fe20f3851e6832c632d9eb35ec4be
ctevnan <carolyntevnan@yahoo.com>
2016-10-13 12:10:50 -0400

    adding a container and row to index.html, adding stylesheet so bootstrap works

--- src/js/components/SearchForm.js
+++ src/js/components/SearchForm.js
@@ -6,12 +6,13 @@ var SearchForm = React.createClass({
     render: function(){
         return(
             <div className="search-form">
-            <h1 className="text-center">Search For A Movie</h1>
-            <form>
-              <div className="form-group">
-                <input type="text" className="form-control" ref="title" placeholder="Enter a Movie Title..." />
-            </form>
-            <button className="btn btn-primary btn-block">Search Movies</button>
+              <h1 className="text-center">Search For A Movie</h1>
+                <form>
+                  <div className="form-group">
+                  <input type="text" className="form-control" ref="title" placeholder="Enter a Movie Title..." />
+                  </div>
+                </form>
+                <button className="btn btn-primary btn-block">Search Movies</button>
             </div>    
         )
     }
