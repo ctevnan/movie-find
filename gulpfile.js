b4f1274791107c95d4d4b706df13b8e89af39fba
ctevnan <carolyntevnan@yahoo.com>
2016-10-13 10:47:26 -0400

    adding node modules, app.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
-//set up gulp tasks
 gulp.task('browserify', function(){
   browserify('./src/js/main.js')
     .transform('reactify')
@@ -12,7 +11,6 @@ gulp.task('browserify', function(){
     .pipe(gulp.dest('dist/js'));
 });
 
-//bring css and vendor js files over
 gulp.task('copy', function(){
   gulp.src('src/index.html')
     .pipe(gulp.dest('dist'));
@@ -21,7 +19,7 @@ gulp.task('copy', function(){
   gulp.src('src/js/vendors/*.*')
     .pipe(gulp.dest('dist/js'));    
 })
-//create default task
+
 gulp.task('default', ['browserify', 'copy'], function(){
   return gulp.watch('src/**/*.*', ['browserify', 'copy']);
 });
