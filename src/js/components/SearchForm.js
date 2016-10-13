1b247cd4072605b9a8dce007bdc08770f885b5dc
ctevnan <carolyntevnan@yahoo.com>
2016-10-13 11:51:06 -0400

    adding search form

---
+++ src/js/components/SearchForm.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+
+var SearchForm = React.createClass({
+    render: function(){
+        return(
+            <div className="search-form">
+            <h1 className="text-center">Search For A Movie</h1>
+            <form>
+              <div className="form-group">
+                <input type="text" className="form-control" ref="title" placeholder="Enter a Movie Title..." />
+            </form>
+            <button className="btn btn-primary btn-block">Search Movies</button>
+            </div>    
+        )
+    }
+});
+
+module.exports = SearchForm;
